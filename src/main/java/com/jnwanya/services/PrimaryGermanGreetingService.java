b8b13860612db06936c0d33f9c4047444b957b35
Nwanya Justin <nwanyajustin@gmail.com>
2018-01-04 23:22:38 +0100

    Demonstrated the use of Default Profile configuration for DI

--- src/main/java/com/jnwanya/services/PrimaryGermanGreetingService.java
+++ src/main/java/com/jnwanya/services/PrimaryGermanGreetingService.java
@@ -10,11 +10,10 @@ import org.springframework.stereotype.Service;
  */
 @Service
 @Primary
-@Profile({"en", "default"})
-public class PrimaryGreetingService implements GreetingService {
-
+@Profile("de")
+public class PrimaryGermanGreetingService implements GreetingService{
     @Override
     public String sayGreeting() {
-        return "Hello from PrimaryGreetingService";
+        return "Grüße aus Deutschland Service";
     }
 }
